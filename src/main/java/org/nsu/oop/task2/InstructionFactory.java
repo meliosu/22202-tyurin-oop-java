2fa8b188660452fd0f0315552d2b539660e552ad
Alexander <a.tyurin@g.nsu.ru>
2024-02-17 10:56:09 +0700

    partially implemented instructions factory

--- src/main/java/org/nsu/oop/task2/InstructionFactory.java
+++ src/main/java/org/nsu/oop/task2/InstructionFactory.java
@@ -1,12 +1,32 @@
 package org.nsu.oop.task2;
 
+import org.nsu.oop.task2.exceptions.InstructionFactoryException;
+
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Stream;
+import java.util.Scanner;
 
 public class InstructionFactory {
     private Map<String, Class<?>> instructionsMap;
 
-    public InstructionFactory() {
+    public InstructionFactory(String configPath) {
+        instructionsMap = new HashMap<>();
 
+        InputStream stream;
+        try {
+            stream = InstructionFactory.class.getResourceAsStream(configPath);
+        } catch (RuntimeException e) {
+            throw new InstructionFactoryException("cannot open instructions config file");
+        }
+
+        Scanner configScanner = new Scanner(stream);
+
+        while (configScanner.hasNext()) {
+            String[] tokens = configScanner.nextLine().split("=");
+            if (tokens.length != 2) {
+                throw new InstructionFactoryException("bad instruction factory format");
+            }
+        }
     }
 }
