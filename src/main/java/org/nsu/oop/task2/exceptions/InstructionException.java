4418c544fbbef992f9fbe52fbf6230ae7e12103d
Alexander <a.tyurin@g.nsu.ru>
2024-04-13 00:02:51 +0700

    better InstructionException error printing

--- src/main/java/org/nsu/oop/task2/exceptions/InstructionException.java
+++ src/main/java/org/nsu/oop/task2/exceptions/InstructionException.java
@@ -4,6 +4,6 @@ import org.nsu.oop.task2.instructions.Instruction;
 
 public class InstructionException extends CalculatorException {
     public InstructionException(String message, Instruction instruction) {
-        super("while executing" + instruction.toString() + "instruction: " + message);
+        super("while executing " + instruction.getClass() + "instruction: " + message);
     }
 }
