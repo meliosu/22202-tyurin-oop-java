8e3c643f9619c1d24ce2ce3c413555d801eac4a1
Alexander <a.tyurin@g.nsu.ru>
2024-04-13 00:03:06 +0700

    main app

--- src/main/java/org/nsu/oop/task2/App.java
+++ src/main/java/org/nsu/oop/task2/App.java
@@ -2,6 +2,12 @@ package org.nsu.oop.task2;
 
 public class App {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
 
+        if (args.length > 1) {
+            calculator.executeFile(args[1]);
+        } else {
+            calculator.executeStdin();
+        }
     }
 }
